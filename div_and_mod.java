de6f88e5a4a9bd4fcfb14ddbfec99076eb864b93
jtmer276 <159070824+jtmer276@users.noreply.github.com>
2024-03-02 20:43:12 -0500

    qwd

---
+++ div_and_mod.java
@@ -0,0 +1,20 @@
+/******************
+ Name: Jules Merrill
+ Date: March 2nd, 2024
+ Project: Div and mod
+ ******************/
+
+import java.util.*;
+
+public class div_and_mod{
+    public void static main(String[] args){
+        int int_one;
+        int int_two;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int_one = input.nextInt();
+        System.out.print("Enter a second integer: /n");
+        int_two = input.nextInt();
+
+    }
+}
